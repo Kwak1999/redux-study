3acd74dce2f01ccb815ba3ec29d5b546141f425a
HuiBeom <gmlqja7913@daum.net>
2025-09-20 01:08:40 +0900

    redux-counter-app, 기본 사용법

--- redux-step1/src/reducers/index.tsx
+++ redux-step1/src/reducers/index.tsx
@@ -1,10 +1,16 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import counter from "./counter";
+// @ts-ignore
 import todos from "./todos";
+import posts from "./posts";
 
 const rootReducer = combineReducers({
     counter,
-    todos
+    todos,
+    posts
 })
 
+export type RootState = ReturnType<typeof rootReducer>;
+
+// ⬇⬇⬇ 여기가 중요
 export default rootReducer;
